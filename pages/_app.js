adbd486f0f83f9f899ea960ece1af27b093b56d9
dfelsie <dfelsent@macalester.edu>
2021-05-20 23:46:59 -0400

    New logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  CartContext,
+  useCartState,
+} from "../hooks/use-cart.solution.js";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <CartContext.Provider value={cart}>
+    return <Component {...pageProps} />
+  </CartContext.Provider>;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,15 @@
-import { useState } from "react";
 import Head from "next/head";
+import { FaShoppingCart } from "react-icons/fa";
 import styles from "../styles/Home.module.css";
-import { Button } from "@material-ui/core";
 
-import { initiateCheckout } from "../libs/payment";
+import { initiateCheckout } from "../lib/payments.js";
 
-import products from "../products.json";
+import useCart from "../hooks/use-cart.solution.js";
 
-const defaultCart = {
-  products: {},
-};
+import products from "../../shared/products.json";
 
 export default function Home() {
-  /**
-   * @lesson-09-solution Exercise 2
-   * Using React's useState hook, we can store the current "state" of
-   * our customer's shopping cart.
-   */
-
-  const [cart, updateCart] = useState(defaultCart);
-
-  const cartItems = Object.keys(cart.products).map(
-    (key) => {
-      const product = products.find(
-        ({ id }) => `${id}` === `${key}`
-      );
-      return {
-        ...cart.products[key],
-        pricePerUnit: product.price,
-      };
-    }
-  );
-
-  const subtotal = cartItems.reduce(
-    (accumulator, { pricePerUnit, quantity }) => {
-      return accumulator + pricePerUnit * quantity;
-    },
-    0
-  );
-
-  const quantity = cartItems.reduce(
-    (accumulator, { quantity }) => {
-      return accumulator + quantity;
-    },
-    0
-  );
-
-  /**
-   * @lesson-09-solution Exercise 3
-   * We can create an addToCart function that given the product ID, adds that item
-   * to our local cart state along with incrementing the quantity of that item by
-   * 1 if it exists or setting it as a new product in our cart if it didn't exist.
-   */
-
-  function addToCart({ id }) {
-    updateCart((prev) => {
-      let cart = { ...prev };
-
-      if (cart.products[id]) {
-        cart.products[id].quantity =
-          cart.products[id].quantity + 1;
-      } else {
-        cart.products[id] = {
-          id,
-          quantity: 1,
-        };
-      }
-
-      return cart;
-    });
-  }
-
-  /**
-   * @lesson-09-solution Exercise 6
-   * The iniateCheckout function can still be used to trigger the checkout
-   * sequence, but now, we're dynamically passing an array of products
-   * that includes each product ID along with how many of that item.
-   */
+  const { subtotal, quantity, addToCart } = useCart();
 
   function checkout() {
     initiateCheckout({
@@ -103,35 +36,23 @@ export default function Home() {
           The best space jellyfish swag on the web!
         </p>
 
-        {/**
-         * @lesson-09-solution Exercise 1
-         * We can make use of the same description block as above to create a simple
-         * section to include our total number of items and the total cost.
-         */}
-
-        <p className={styles.description}>
-          {/**
-           * @lesson-09-solution Exercise 4
-           * We're storing our cart state inside of the cart object. We can use that
-           * data to calculate how many items are in our cart as well as what the total
-           * cost of all of those items are.
-           */}
-          <strong>Items:</strong> {quantity}
-          <br />
-          <strong>Total:</strong> ${subtotal}
-          {/**
-           * @lesson-09-solution Exercise 6
-           * We can add a new button, link, or even make the entire paragraph a link that
-           * triggers out checkout button to initiate the checkout sequence.
-           */}
-          <br />
-          <button
-            className={styles.button}
-            onClick={checkout}
-          >
-            Check Out
-          </button>
-        </p>
+        <ul className={styles.cart}>
+          <li>
+            <strong>Items:</strong> {quantity}
+          </li>
+          <li>
+            <strong>Total:</strong> ${subtotal}
+          </li>
+          <li>
+            <button
+              className={`${styles.button} ${styles.cartButton}`}
+              onClick={checkout}
+            >
+              <FaShoppingCart />
+              Check Out
+            </button>
+          </li>
+        </ul>
 
         <ul className={styles.grid}>
           {products.map((product) => {
@@ -139,24 +60,20 @@ export default function Home() {
               product;
             return (
               <li key={id} className={styles.card}>
-                <img src={image} alt={title} />
-                <h3>{title}</h3>
-                <p>${price}</p>
-                <p>{description}</p>
-                <p>
-                  {/**
-                   * @lesson-09-solution Exercise 5
-                   * The initiateCheckout function can be swapped with the addToCart function
-                   * while still including the ID as an argument, allowing us to use that ID
-                   * to add the product to our shopping cart.
-                   */}
-                  <Button
-                    className={styles.button}
-                    onClick={() => addToCart({ id })}
-                  >
-                    Buy
-                  </Button>
-                </p>
+                <a href="#">
+                  <img src={image} alt={title} />
+                  <h3>{title}</h3>
+                  <p>${price}</p>
+                  <p>{description}</p>
+                  <p>
+                    <button
+                      className={styles.button}
+                      onClick={() => addToCart({ id })}
+                    >
+                      Buy
+                    </button>
+                  </p>
+                </a>
               </li>
             );
           })}
